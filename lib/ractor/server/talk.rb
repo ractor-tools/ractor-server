f247e07720f2e049b4e222e502adbbf269c4ebcf
Marc-Andre Lafortune <github@marc-andre.ca>
2021-01-24 12:40:20 -0500

    Use `refine_export` gem

--- lib/ractor/server/talk.rb
+++ lib/ractor/server/talk.rb
@@ -1,27 +1,8 @@
 # frozen_string_literal: true
 # shareable_constant_value: literal
 
-module RefinementExporter
-  refine Module do
-    # See https://bugs.ruby-lang.org/issues/17374#note-8
-    def refine(what, export: false)
-      mod = super(what)
-      return mod unless export
-
-      export = self if export == true
-      export.class_eval do
-        mod.instance_methods(false).each do |method|
-          define_method(method, mod.instance_method(method))
-        end
-        mod.private_instance_methods(false).each do |method|
-          private define_method(method, mod.instance_method(method))
-        end
-      end
-      mod
-    end
-  end
-end
-using RefinementExporter
+require 'refine_export'
+using RefineExport
 
 class Ractor
   module Server
--- ractor-server.gemspec
+++ ractor-server.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
 
   # Uncomment to register a new dependency of your gem
   spec.add_dependency 'require_relative_dir', '>= 1.1.0'
+  spec.add_dependency 'refine_export'
 
   # For more information and examples about making a new gem, checkout our
   # guide at: https://bundler.io/guides/creating_gem.html
